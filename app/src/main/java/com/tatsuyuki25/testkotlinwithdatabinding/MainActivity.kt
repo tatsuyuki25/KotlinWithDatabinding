da392e72a81849f9f41ffd50783c161a57f18832
tatsuyuki <tatsuyuki@program.com.tw>
2017-06-07 09:35:26 +0800

    init

--- app/src/main/java/com/tatsuyuki25/testkotlinwithdatabinding/MainActivity.kt
+++ app/src/main/java/com/tatsuyuki25/testkotlinwithdatabinding/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.tatsuyuki25.testkotlinwithdatabinding
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.tatsuyuki25.testkotlinwithdatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //add new view in activity_main_xml
     }
 }
